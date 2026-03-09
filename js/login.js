e538a1790cd62d0c412cd07e81af4e4ab3e0cd0e
kolyanhabib <kolyanhabib@gmail.com>
2026-03-09 20:25:18 +0600

    build the logic of the login page and make it dynamic

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const loginBtn = document.getElementById("loginBtn");
+const errorMsg = document.getElementById("errorMsg");
+
+loginBtn.addEventListener("click", function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    localStorage.setItem("loggedIn", "true"); // important line
+
+    alert("Log In Successful");
+
+    window.location.href = "index.html";
+  } else {
+    errorMsg.classList.remove("hidden");
+  }
+});
